139bef6003df08018f6f1d53ffb466d5f5e7f5de
Zakhar Kogan <36503576+zaharkogan@users.noreply.github.com>
2023-01-10 15:50:42 +0200

    Initial commit

---
+++ scrapy_numbeo_ccli/settings.py
@@ -0,0 +1,5 @@
+# default settings for scrapy
+BOT_NAME = 'scrapy_numbeo_ccli'
+
+SPIDER_MODULES = ['crapy_numbeo_ccli.spider']
+NEWSPIDER_MODULE = 'crapy_numbeo_ccli.spider'
---
+++ scrapy_numbeo_ccli/spider.py
@@ -0,0 +1,35 @@
+import scrapy
+
+class SpiderSpider(scrapy.Spider):
+    name = 'spider'
+    allowed_domains = ['numbeo.com']
+    start_urls = ['https://www.numbeo.com/cost-of-living/rankings_current.jsp']
+
+    def parse(self, response):
+        table = response.xpath('//*[@id="t2"]')
+        rows = table.xpath('//tr')
+        
+        city_country_strs = [item for item in rows.xpath('td[2]/a/text()').extract()]
+        # code below 
+        city, country = zip(*[item.split(', ') for item in city_country_strs])
+        links = rows.xpath('td[2]/a/@href').extract()
+        col_index = [float(item) for item in rows.xpath('td[3]/text()').extract()]
+        rent_index = [float(item) for item in rows.xpath('td[4]/text()').extract()]
+        col_rent_index = [float(item) for item in rows.xpath('td[5]/text()').extract()]
+        groceries_index = [float(item) for item in rows.xpath('td[6]/text()').extract()]
+        restaurant_price_index = [float(item) for item in rows.xpath('td[7]/text()').extract()]
+        local_purchasing_power_index = [float(item) for item in rows.xpath('td[8]/text()').extract()]
+        
+        for city, country, links, col_index, rent_index, col_rent_index, groceries_index, restaurant_price_index, local_purchasing_power_index in zip(city, country, links, col_index, rent_index, col_rent_index, groceries_index, restaurant_price_index, local_purchasing_power_index):
+            yield {
+                'city': city,
+                'country': country,
+                'links': links,
+                'col_index': col_index,
+                'rent_index': rent_index,
+                'col_rent_index': col_rent_index,
+                'groceries_index': groceries_index,
+                'restaurant_price_index': restaurant_price_index,
+                'local_purchasing_power_index': local_purchasing_power_index,
+            }
+        pass
